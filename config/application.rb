ede4c07ae61c66370292987b79bff9f525a3fe98
Jackson Pires <jack@jack.com>
2019-06-03 18:05:26 +0000

    Corrigindo o glyphicons

--- config/application.rb
+++ config/application.rb
@@ -30,5 +30,8 @@ module TimeToAnswer
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    # Bootstrap Glyphicons Fix
+    config.assets.paths << Rails.root.join('node_modules','bootstrap','fonts')
   end
 end
